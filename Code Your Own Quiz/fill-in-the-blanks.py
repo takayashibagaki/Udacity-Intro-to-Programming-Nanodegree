4c0bc9ce4a14040f693891b2415e3041634f9919
Takaya <kametann55@gmail.com>
2022-12-15 17:55:09 +0900

    Add files via upload

---
+++ Code Your Own Quiz/fill-in-the-blanks.py
@@ -0,0 +1,37 @@
+#the easy empty fill-in-the-blank
+easy_q=["President of the United States who won a dramatic victory
+         in the 2016 presidential election is",
+        "__1__", "__2__", "serving as German prime minister for 12 years"
+        "Japan's prime minister, who is setting out an economic policy called abenomics, is ",
+        "__3__", "Chinese President is","__4__","__5__",
+        "was elected President of France at the young age of 39 years old."]
+
+easy_answers=["Trunp","Merkel","Abe","Xi","Macron"]
+
+#the mediun empty fill-in-the-blank
+medium_q=["__1__","invented an innovative device such as an iPhone.",
+          "__2__","offers a wide range of services such as e-mail from search engine.",
+          "__3__","will deliver it as soon as you place an order.",
+          "__4__","is making the latest electric cars and managed by Eelon Mask.",
+          "__5__","is the world's largest SNS, and it has become an indispensable part of our lives.",
+          "__6__","is a world-class automobile manufacturer in Japan and the Prius is famous.",
+          "__7__","has developed Windows and has influence on PCs in the world.",
+
+]
+
+medium_answers["Apple","Google","Amazon","Tesla", "Facebook","Toyota","Microsoft",]
+
+# the hard empty fill-in-the-blank
+
+hard_q=["Japanese food",
+       "__1__","is a combination of rice and seafood.",
+       "__2__" "is fried with vegetables and fish and shellfish in a clothed flour and egg combined.",
+       "__3__","is a processed food made by soybean squeezed with coagulant."
+       "__4__", "is a noodle made from wheat flour, and it is sometimes eaten with tempura etc.",
+       "__5__", "is a noodle made from buckwheat flour, which is similar to but different from udon."
+       "__6__", "is based on Chinese noodles and soup, with various ingredients such as chashu, menma, Tamago etc",
+       "__7__", "is the most commonly drunk beverage in Japan."
+       "It is a drink made by processing tea tree leaves and stems."
+]
+
+hard_answers=["Sushi","Tempura","Tofu","Udon","Soba","Ramen","Tea",]
